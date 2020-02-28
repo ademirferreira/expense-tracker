b3139c28447ca0e83ff67991bac8a83437084c07
Ademir Ferreira <ademir.ae86@gmail.com>
2020-02-27 22:16:58 -0300

    Add display balance, income and expense

--- script.js
+++ script.js
@@ -29,10 +29,32 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expenses = (
+    amounts.filter(item => item < 0)
+      .reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+
+  balance.innerText = `R$${total}`;
+  money_plus.innerText = `R$${income}`;
+  money_minus.innerText = `R$${expenses}`;
+
+}
+
 function init() {
   list.innerHTML = '';
 
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
